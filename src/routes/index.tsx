7c61277b4b058f8a24e569ecd526b2577909c5d2
arezhz <arezhz@gmail.com>
2024-08-02 05:40:56 +0330

    redirect to login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,12 +12,6 @@ export default function AppRoutes() {
 
         {/*<Route element={<PrivateRoute />}>
           <Route path="dashboard/*" element={<DashboardRoutes />} />
-          <Route path="guests/*" element={<GuestsRoutes />} />
-          <Route path="admin/*" element={<AdminsRouts />} />
-          <Route path="fast-send/*" element={<FastSendTemplateRoutes />} />
-          <Route path="sms-queue/*" element={<SMSQueueRoutes />} />
-          <Route path="ticket/*" element={<TicketRoutes />} />
-          <Route path="users/*" element={<UsersRoutes />} />
         </Route> */}
       </Routes>
     </BrowserRouter>
